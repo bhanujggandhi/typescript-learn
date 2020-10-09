21bcf95404dbe89f550f9d949f6d0af1e53a3690
bhanujggandhi <gandhibhanuj@gmail.com>
2020-10-09 19:44:37 +0530

    keeper app added, ts types installed

---
+++ keeper-app-ts/src/components/Header.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import EmojiObjectsOutlinedIcon from '@material-ui/icons/EmojiObjectsOutlined';
+
+function Header() {
+	return (
+		<header>
+			<h1>
+				<EmojiObjectsOutlinedIcon fontSize = "large"/>
+				Keeper
+			</h1>
+		</header>
+	);
+}
+
+export default Header;
